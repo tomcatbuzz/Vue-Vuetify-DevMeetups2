b430bd72fc9bf73b0ec305f2c08c3160e4866397
Tony <tomcatbuzz@yahoo.com>
2019-06-23 21:00:45 -0400

    initial rebuild

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,89 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer absolute temporary v-model="sideNav">
+      <v-list>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile
+          v-if="userIsAuthenticated"
+          @click="onLogout">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>Logout</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">Dev Meetups</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+      <v-btn flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link">
+        <v-icon left dark>{{ item.icon}}</v-icon>
+        {{ item.title }}
+      </v-btn>
+      <v-btn
+        v-if="userIsAuthenticated"
+        flat
+        @click="onLogout">
+        <v-icon left dark>exit_to_app</v-icon>
+        Logout
+      </v-btn>
+    </v-toolbar-items>
+    </v-toolbar>
+    <main>
+      <router-view></router-view>
+    </main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false
+    }
+  },
+  computed: {
+    menuItems () {
+      let menuItems = [
+        { icon: 'face', title: 'Sign up', link: '/signup' },
+        { icon: 'lock_open', title: 'Sign in', link: '/signin' }
+      ]
+      if (this.userIsAuthenticated) {
+        menuItems = [
+        { icon: 'supervisor_account', title: 'View Meetups', link: '/meetups' },
+        { icon: 'room', title: 'Organize Meetup', link: '/meetup/new' },
+        { icon: 'person', title: 'Profile', link: '/profile' }
+        ]
+      }
+      return menuItems
+    },
+    userIsAuthenticated () {
+      return this.$store.getters.user !== null && this.$store.getters.user !== undefined
+    }
+  },
+  methods: {
+    onLogout () {
+      this.$store.dispatch('logout')
     }
   }
 }
+</script>
+
+<style>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import { store } from './store';
 import './registerServiceWorker';
+import * as firebase from 'firebase';
 
 Vue.config.productionTip = false;
 
@@ -10,4 +12,20 @@ new Vue({
   router,
   store,
   render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'YOUR KEY HERE',
+      authDomain: 'vue-devmeetups.firebaseapp.com',
+      databaseURL: 'https://vue-devmeetups.firebaseio.com',
+      projectId: 'vue-devmeetups',
+      storageBucket: 'vue-devmeetups.appspot.com'
+    });
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$store.dispatch('autoSignIn', user);
+        this.$store.dispatch('fetchUserData');
+      }
+    });
+    this.$store.dispatch('loadMeetups');
+  },
 }).$mount('#app');
